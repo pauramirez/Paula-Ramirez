e0df602fb63c2da683e450a7ab3612dd5c0019b0
Paula Ramirez <paularamirez@Paus-MacBook-Pro.local>
2018-02-08 11:45:57 -0500

    avances

--- scriptt.js
+++ scriptt.js
@@ -14,16 +14,30 @@ function currentSlide(n) {
 function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("mySlides");
+  var slides1 = document.getElementsByClassName("mySlides1");
   var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1} 
+  if (n > slides.length) {slideIndex = 0} 
   if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
+  for (i = 0; i <= slides.length; i++) {
       slides[i].style.display = "none"; 
   }
   for (i = 0; i < dots.length; i++) {
       dots[i].className = dots[i].className.replace(" active", "");
   }
   slides[slideIndex-1].style.display = "block"; 
+  dots[slideIndex-1].className += " active";
+
+
+//para la segunda seccion
+  if (n > slides1.length) {slideIndex = 1} 
+  if (n < 1) {slideIndex = slides1.length}
+  for (i = 0; i < slides1.length; i++) {
+      1[i].style.display = "none"; 
+  }
+  for (i = 0; i <= dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides1[slideIndex-1].style.display = "block"; 
   dots[slideIndex-1].className += " active";
 }
 
@@ -33,11 +47,41 @@ showSlides();
 function showSlides() {
     var i;
     var slides = document.getElementsByClassName("mySlides");
+    var slides1 = document.getElementsByClassName("mySlides1");
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none"; 
     }
     slideIndex++;
     if (slideIndex > slides.length) {slideIndex = 1} 
     slides[slideIndex-1].style.display = "block"; 
-    setTimeout(showSlides, 3000); // Change image every 2 seconds
+    setTimeout(showSlides, 4000); // Change image every 2 seconds
+
+//para la segunda seccion
+	for (i = 0; i < slides1.length; i++) {
+        slides1[i].style.display = "none"; 
+    }
+    slideIndex++;
+    if (slideIndex > slides1.length) {slideIndex = 1} 
+    slides1[slideIndex-1].style.display = "block"; 
+    setTimeout(showSlides, 4000); // Change image every 2 seconds
+}
+
+var slideIndex = [1,1];
+var slideId = ["mySlides1", "mySlides2"]
+showSlides(1, 0);
+showSlides(1, 1);
+
+function plusSlides(n, no) {
+  showSlides(slideIndex[no] += n, no);
+}
+
+function showSlides(n, no) {
+  var i;
+  var x = document.getElementsByClassName(slideId[no]);
+  if (n > x.length) {slideIndex[no] = 1}    
+  if (n < 1) {slideIndex[no] = x.length}
+  for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";  
+  }
+  x[slideIndex[no]-1].style.display = "block";  
 }
